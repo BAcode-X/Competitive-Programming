4c71f0a41501c8097af11d61523a932f4b51895b
Biruk Alamirew <biruk.alamirew77@gmail.com>
2022-02-06 11:33:28 +0300

    return the first occurance of a string

---
+++ implement-strstr.py
@@ -0,0 +1,11 @@
+"""
+    Return the index of the first occurrence of needle in haystack, 
+    or -1 if needle is not part of haystack.
+"""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+            return haystack.index(needle)
+        except ValueError:
+            return -1
