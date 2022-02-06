223dade6404422e884b47ed0392f53515387aabb
Biruk Alamirew <biruk.alamirew77@gmail.com>
2022-02-06 11:27:47 +0300

    removing element from an array

---
+++ remove-element.py
@@ -0,0 +1,16 @@
+"""
+    Given an integer array nums and an integer val, 
+    remove all occurrences of val in nums in-place. 
+    The relative order of the elements may be changed.
+"""
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        cnt = 0
+        n = nums.__len__()
+        if n:
+            for i in range(n):
+                if nums[i] != val:
+                    nums[cnt] = nums[i]
+                    cnt += 1
+        return cnt
