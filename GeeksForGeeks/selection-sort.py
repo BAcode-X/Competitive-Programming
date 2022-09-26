b5a9cc0efeec154db0246f21b54af5bac84f13cd
Biruk <biruk.alamirew77@gmail.com>
2022-09-26 19:26:38 +0300

    geeksforgeeks: added selection sort solution

---
+++ GeeksForGeeks/selection-sort.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    a = Counter(arr)
+    return [a.get(i, 0) for i in range(100)]
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
